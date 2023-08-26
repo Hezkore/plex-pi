15ed3f8841535e8067dc75963e5f552c79ceee08
Hezkore <hezkore@gmail.com>
2023-08-26 15:04:29 +0200

    updated qbit script

--- scripts/qbittorrent_remove_unfinished.sh
+++ scripts/qbittorrent_remove_unfinished.sh
@@ -7,80 +7,83 @@
 # 0 * * * * /path/to/your/script.sh --username=<YOUR_QBITTORRENT_USERNAME> --password=<YOUR_QBITTORRENT_PASSWORD> > /dev/null 2>&1
 
 QB_URL="http://localhost:8080"
-DEFAULT_THRESHOLD=6  # Default time threshold in hours
+DEFAULT_THRESHOLD=6
 
-# Fetch list of torrents from qBittorrent Web API
+for i in "$@"
+do
+	case $i in
+		--username=*)
+		QB_USERNAME="${i#*=}"
+		shift
+		;;
+		--password=*)
+		QB_PASSWORD="${i#*=}"
+		shift
+		;;
+		-t=*|--time=*)
+		time_threshold="${i#*=}"
+		shift
+		;;
+		*)
+		echo "Unknown parameter: $1"
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "$time_threshold" ]; then
+	time_threshold=$DEFAULT_THRESHOLD
+fi
+
+if [ -z "$QB_USERNAME" ] || [ -z "$QB_PASSWORD" ]; then
+	echo "Please provide qBittorrent username and password."
+	exit 1
+fi
+
 fetch_qb_torrents() {
 	curl -sX GET "$QB_URL/api/v2/torrents/info?filter=all" -u "$QB_USERNAME:$QB_PASSWORD"
 }
 
-# Remove a torrent from qBittorrent Web API by its hash
 remove_qb_torrent() {
  	response=$(curl -sX GET "$QB_URL/api/v2/torrents/delete?hashes=$1&deleteFiles=true" -u "$QB_USERNAME:$QB_PASSWORD")
 }
 
-main() {
-	for i in "$@"
-	do
-		case $i in
-			--username=*)
-			QB_USERNAME="${i#*=}"
-			shift
-			;;
-			--password=*)
-			QB_PASSWORD="${i#*=}"
-			shift
-			;;
-			-t=*|--time=*)
-			DEFAULT_THRESHOLD="${i#*=}"
-			shift
-			;;
-			*)
-			echo "Unknown parameter: $1"
-			exit 1
-			;;
-		esac
-	done
+echo "Fetching active torrents..."
+torrents=$(fetch_qb_torrents)
+
+if [ -z "$torrents" ]; then
+	echo "No active torrents found."
+	exit 0
+fi
+
+#echo "Torrents: $torrents"
+# Loop thorugh each torrent
+echo "Removing torrents active for more than $time_threshold hours..."
+echo "$torrents" | jq -r '.[] | "\(.name),\(.added_on),\(.hash),\(.state)"' |
+while IFS=',' read -r name added_on hash state; do
+	# Process each torrent
+	#echo "Processing torrent: $name"
+	#echo "State: $state"
+	timestamp=$(date +%s)
 	
-	torrents=$(fetch_qb_torrents)
+	# Calculate the time difference in hours
+	time_diff=$(( (timestamp - added_on) / 3600 ))
 	
-	if [ -z "$torrents" ]; then
-		echo "No active torrents found."
-		exit 0
+	# Reomve any stalled torrents
+	if [ "$state" == "stalledUP" ] || [ "$state" == "stalledDL" ]; then
+		echo "Removing (stalled) $name"
+	  	remove_qb_torrent $hash
+		#removed_count=$((removed_count+1))
+		continue
 	fi
 	
-	time_threshold=${DEFAULT_THRESHOLD}
-	
-	echo "Removing torrents active for more than $time_threshold hours..."
-	
-	# Loop through each torrent in JSON
-	while true; do
-		torrents=$(fetch_qb_torrents)
-		removed_count=0
-		
-		for i in $(jq -r '.[] | @base64' <<< "$torrents"); do
-			_jq() {
-				echo "${i}" | base64 --decode | jq -r "${1}"
-			}
-			
-			added_on=$(_jq '.added_on')
-			time_diff=$(( ($(date +%s) - $added_on) / 3600 ))
-			
-			if [ $time_diff -ge $time_threshold ] && [ $(_jq '.state') != "queuedDL" ] && [ $(_jq '.state') != "queuedUP" ]; then
-				hash=$(_jq '.hash')
-				name=$(_jq '.name')
-				echo "Removing $name..."
-				remove_qb_torrent "$hash"
-				removed_count=$((removed_count+1))
-			fi
-		done
-		
-		if [ $removed_count -eq 0 ]; then
-			break
-		fi
-	done
-	
-	echo "Torrent removal complete."
-}
+	# Remove any torrents that meet our time threshold
+	if [ $time_diff -ge $time_threshold ]; then
+		echo "Removing (time) $name"
+	  	remove_qb_torrent $hash
+		#removed_count=$((removed_count+1))
+		continue
+	fi
+done
 
-main "$@"
+#echo "Removed $removed_count torrents."
